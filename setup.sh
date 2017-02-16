5de9359436f15471f09df790f37dff3c5c404c69
Elvis Oliveira <elvis.olv@gmail.com>
2017-02-16 13:15:11 -0200

    Wordpress installation on base enviroment.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-alias wp="/usr/bin/wp --allow-root"
+# docker exec odyssey-webserver /bin/sh -c "/var/www/odyssey/setup.sh"
+
+# Database
+
 
 # WordPress Config
-wp core download
+wp --allow-root core config --dbhost=database \
+                            --dbname=translation-odyssey \
+                            --dbuser=root \
+                            --dbpass=123
+
+# WordPress Install
+wp --allow-root core install --url=http://localhost/ \
+                             --title='Translation Odyssey' \
+                             --admin_user=admin \
+                             --admin_password=123 \
+                             --admin_email=elvis.olv@gmail.com
