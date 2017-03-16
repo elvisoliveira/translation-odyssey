649a7f6aa812c6c7e691323f8f88471f630f6a2c
Elvis Oliveira <elvis.olv@gmail.com>
2017-03-16 16:23:25 -0300

    Content setup improvements. Generate ACF contents.

---
+++ .docker/wordpress/post-content.php
@@ -0,0 +1,8 @@
+<?php
+
+$field['WP_INDX'] = $args[0];
+$field['WP_TEMP'] = $args[1];
+$field['WP_ATTC'] = $args[2];
+$field['WP_POST'] = $args[3];
+
+update_field('field_58c15679c1b2a', $field['WP_ATTC'], $field['WP_POST']);
--- setup.sh
+++ setup.sh
@@ -2,40 +2,35 @@
 # docker exec -i -t odyssey-webserver /bin/sh -c "/var/www/odyssey/setup.sh"
 
 # Node.js resources
-(cd ./wp-content/themes/odyssey/node && npm run assets)
+read -p "Execute NPM? [yes] " npm
 
-# WordPress Download
-if [ ! -f "wp-settings.php" ]; then
-    wp --allow-root core download # --locale=pt_BR
+while [[ -z "$npm" ]]; do
+    npm="yes"
+done
+
+if [ "$npm" == "yes" ]; then
+  (cd ./wp-content/themes/odyssey/node && npm run assets)
 fi
 
-# WordPress Config
+# WordPress Install ############################################################
+################################################################################
+
+# Download
+if [ ! -f "wp-settings.php" ]; then
+    wp core download --allow-root # --locale=pt_BR
+fi
+
+# Config
 if [ ! -f "wp-config.php" ]; then
     # DB Config on docker-compose.yml
-    wp --allow-root core config --dbhost=database \
-                                --dbname=$MYSQL_DATABASE \
-                                --dbuser=$MYSQL_USER \
-                                --dbpass=$MYSQL_PASSWORD
+    wp core config --dbhost=database \
+                   --dbname=$MYSQL_DATABASE \
+                   --dbuser=$MYSQL_USER \
+                   --dbpass=$MYSQL_PASSWORD \
+                   --allow-root
 fi
 
-# WordPress: Post Types
-
-## Team
-if [ ! -f "wp-content/themes/odyssey/post-types/team.php" ]; then
-    wp --allow-root scaffold post-type team --theme='odyssey' --label='Team Member' --dashicon='id-alt'
-fi
-
-## Banner
-if [ ! -f "wp-content/themes/odyssey/post-types/banner.php" ]; then
-    wp --allow-root scaffold post-type banner --theme='odyssey' --label='Banner' --dashicon='images-alt'
-fi
-
-## Services
-if [ ! -f "wp-content/themes/odyssey/post-types/services.php" ]; then
-    wp --allow-root scaffold post-type services --theme='odyssey' --label='Service' --dashicon='cart'
-fi
-
-# WordPress Install
+# Install
 while [[ -z "$admin_user" ]]; do
     read -p "[WP] User: " admin_user
 done
@@ -48,33 +43,117 @@ while [[ -z "$admin_email" ]]; do
     read -p "[WP] Email: " admin_email
 done
 
-wp --allow-root core install --url=http://translation-odyssey/ \
-                             --title='Translation Odyssey' \
-                             --admin_user=$admin_user \
-                             --admin_password=$admin_password \
-                             --admin_email=$admin_email \
-                             --skip-email # Avoid postmail: 'sh: 1: -t: not found'
+wp core install --url=http://translation-odyssey/ \
+                --title='Translation Odyssey' \
+                --admin_user=$admin_user \
+                --admin_email=$admin_email \
+                --admin_password=$admin_password \
+                --allow-root \
+                --skip-email # Avoid postmail: 'sh: 1: -t: not found'
 
-# WordPress: ACF
-wp --allow-root plugin install advanced-custom-fields --activate
+# WordPress Setup ##############################################################
+################################################################################
 
-# WordPress: Switch theme.
-wp --allow-root theme activate odyssey
+# Post Type: Team
+if [ ! -f "wp-content/themes/odyssey/post-types/team.php" ]; then
+    wp scaffold post-type team --theme='odyssey' \
+                               --label='Team Member' \
+                               --dashicon='id-alt' \
+                               --allow-root
+fi
 
-# WordPress: Delete default post.
-wp --allow-root site empty --yes
+# Post Type: Banner
+if [ ! -f "wp-content/themes/odyssey/post-types/banner.php" ]; then
+    wp scaffold post-type banner --theme='odyssey' \
+                                 --label='Banner' \
+                                 --dashicon='images-alt' \
+                                 --allow-root
+fi
 
-# WordPress: Create pages.
-wp --allow-root post create ./.docker/wordpress/post-content.txt --post_type='page' --post_status='publish' --post_title='Equipe'
-wp --allow-root post create ./.docker/wordpress/post-content.txt --post_type='page' --post_status='publish' --post_title='Servi&ccedil;os'
-wp --allow-root post create ./.docker/wordpress/post-content.txt --post_type='page' --post_status='publish' --post_title='Blog'
-wp --allow-root post create ./.docker/wordpress/post-content.txt --post_type='page' --post_status='publish' --post_title='Contato'
+# Post Type: Services
+if [ ! -f "wp-content/themes/odyssey/post-types/services.php" ]; then
+    wp scaffold post-type services --theme='odyssey' \
+                                   --label='Service' \
+                                   --dashicon='cart' \
+                                   --allow-root
+fi
 
-# WordPress: Home menu
-wp --allow-root menu create "Home"
+# Plugins: ACF
+wp plugin install advanced-custom-fields --activate --allow-root
 
-# WordPress: Home menu itens
+# Theme: Odyssey
+wp theme activate odyssey --allow-root
+
+# WordPress Content: Post ######################################################
+################################################################################
+
+# Delete default post.
+wp site empty --yes --allow-root
+
+# Page: Team
+wp post create ./.docker/wordpress/post-content.txt --allow-root \
+                                                    --post_type='page' \
+                                                    --post_status='publish' \
+                                                    --post_title='Equipe'
+
+# Page: Services
+wp post create ./.docker/wordpress/post-content.txt --allow-root \
+                                                    --post_type='page' \
+                                                    --post_status='publish' \
+                                                    --post_title='Servi&ccedil;os'
+
+# Page: Blog
+wp post create ./.docker/wordpress/post-content.txt --allow-root \
+                                                    --post_type='page' \
+                                                    --post_status='publish' \
+                                                    --post_title='Blog'
+
+# Page: Contact
+wp post create ./.docker/wordpress/post-content.txt --allow-root \
+                                                    --post_type='page' \
+                                                    --post_status='publish' \
+                                                    --post_title='Contato'
+
+# WordPress Content: Menu ######################################################
+################################################################################
+
+wp menu create "Home" --allow-root
+
+# Menu: Item
 wp --allow-root menu item add-custom home 'Blog' / --target=blog
 wp --allow-root menu item add-custom home 'Contato' / --target=contact
 wp --allow-root menu item add-custom home 'Equipe' / --target=team
 wp --allow-root menu item add-custom home 'Servi&ccedil;os' / --target=services
+
+# WordPress Content: Banner ####################################################
+################################################################################
+
+WP_TEMP=$(mktemp -d)
+
+for WP_INDX in {1..5}; do
+
+    # Temp location
+    WP_IMAG="${WP_TEMP}/${WP_INDX}.jpg"
+
+    # Download 5 ramdom images 1000x1000
+    curl -o ${WP_IMAG} 'https://unsplash.it/1000/1000/?random'
+
+    # Attach on WordPress
+    WP_ATTC=$(wp media import ${WP_IMAG} --porcelain --allow-root)
+
+    # Posts
+    WP_NODE=("wp post create --post_type='banner'      " # Type
+             "               --post_status='publish'   " # Status
+             "               --post_title='${WP_IMAG}' " # Title: Unsupported spaces
+             "               --porcelain               " # Return
+             "               --allow-root              ")
+
+    WP_POST=$(${WP_NODE[@]})
+
+    # ACF
+    wp eval-file ./.docker/wordpress/post-content.php --allow-root \
+                                                      $WP_INDX \
+                                                      $WP_TEMP \
+                                                      $WP_ATTC \
+                                                      $WP_POST
+done
