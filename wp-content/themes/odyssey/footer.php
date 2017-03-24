34fbd0e3857290feaccbdd6b181d3ffaa05638d4
Elvis Oliveira <elvis.olv@gmail.com>
2017-03-24 13:46:33 -0300

    Improviments on the contact form on the footer.

--- wp-content/themes/odyssey/footer.php
+++ wp-content/themes/odyssey/footer.php
@@ -1,17 +1,15 @@
-<?php $contact = new WP_Query(array('page_id' => 5)); ?>
 <div id="about-us">
     <dl>
         <dt>About us</dt>
         <dd><?php print get_post(array('name' => 'about'))->post_content; ?></dd>
     </dl>
 </div>
-<?php if ($contact->have_posts()): ?>
 <div id="contact">
-    <?php while ($contact->have_posts()): ?>
     <dl>
         <dt>Contact</dt>
-        <dd><?php the_post(); ?><?php the_content(); ?></dd>
+        <dd><?php print do_shortcode(get_post(5)->post_content); ?></dd>
     </dl>
-    <?php die(); endwhile; ?>
 </div>
-<?php endif; ?>
+<div id="facebook">
+    <p>Facebook</p>
+</div>
