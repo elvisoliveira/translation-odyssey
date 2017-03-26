b70c988425e5c2f994f9b09117f91fd8a67bc8d4
Elvis Oliveira <elvis.olv@gmail.com>
2017-03-26 17:56:03 -0300

    get_post function works only with post ID and Object, get_page_by_path is more suitable for this implementation

--- wp-content/themes/odyssey/footer.php
+++ wp-content/themes/odyssey/footer.php
@@ -1,13 +1,13 @@
-<div id="about-us">
+<div id="about">
     <dl>
         <dt>About us</dt>
-        <dd><?php print get_post(array('name' => 'about'))->post_content; ?></dd>
+        <dd><?php print get_page_by_path('about')->post_content; ?></dd>
     </dl>
 </div>
 <div id="contact">
     <dl>
         <dt>Contact</dt>
-        <dd><?php print do_shortcode(get_post(5)->post_content); ?></dd>
+        <dd><?php print do_shortcode(get_page_by_path('contact')->post_content); ?></dd>
     </dl>
 </div>
 <div id="facebook">
