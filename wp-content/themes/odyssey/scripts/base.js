5aeb5ad91905508078a26507a9cfca39e8842378
Elvis Oliveira <elvis.olv@gmail.com>
2017-03-26 20:47:15 -0300

    Animated header links

--- wp-content/themes/odyssey/scripts/base.js
+++ wp-content/themes/odyssey/scripts/base.js
@@ -1,11 +1,18 @@
-(function ($) {
+(function($) {
     // Home: Banner
     $('div.banner div.images ul').cycle();
     // Home: Team
-    $('div.team div.team-members').cycle({ 
-        prev:   '.prev', 
-        next:   '.next', 
-        timeout: 0 
+    $('div.team div.team-members').cycle({
+        prev: '.prev',
+        next: '.next',
+        timeout: 0
+    });
+    // Home: Stcky menu
+    $('div.nav a').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: ($('div' + $(this).attr('href')).offset().top) - 60
+        }, 1000);
     });
     $(window).scroll(function($) {
         // Stick navigation to the top of the page
