c3c054ae9b12b8e821b78c4bf11d66bb34a8c12b
Elvis Oliveira <elvis.olv@gmail.com>
2017-03-29 22:17:10 -0300

    Fix sticky header for logged admin user

--- wp-content/themes/odyssey/scripts/base.js
+++ wp-content/themes/odyssey/scripts/base.js
@@ -2,7 +2,7 @@
     // Home: Banner
     $('body.home div.banner div.images ul').cycle();
     // Home: Team
-    $('body.home div.team div.team-members').cycle({
+    $('body.home div#team div.team-members').cycle({
         prev: '.prev',
         next: '.next',
         timeout: 0
@@ -11,13 +11,14 @@
     $('body.home div.nav a').click(function(event) {
         event.preventDefault();
         $('html, body').animate({
-            scrollTop: ($('div' + $(this).attr('href')).offset().top) - 60
+            scrollTop: ($('div' + $(this).attr('href')).offset().top) - (jQuery('body').hasClass('logged-in') ? 82 : 60)
         }, 1000);
     });
     $(window).scroll(function($) {
         // Stick navigation to the top of the page
         var scrollWindow = jQuery(window).scrollTop();
-        var scrollBanner = jQuery('div.banner').offset().top;
-        jQuery('div.nav').removeClass('sticky-header').addClass(scrollWindow > scrollBanner ? 'sticky-header' : '');
+        var scrollOffset = jQuery('body').hasClass('logged-in') ? 32 : 0;
+        var scrollBanner = jQuery('div.banner').offset().top - scrollOffset;
+        jQuery('div.nav').removeClass('sticky-header admin-sticky-header').addClass(scrollWindow > scrollBanner ? jQuery('body').hasClass('logged-in') ? 'admin-sticky-header' : 'sticky-header' : '');
     });
 })(jQuery);
